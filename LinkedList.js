ad8c679013e1190d63a3ab26b163391edda8b859
Andrew Smith <meuzishun@gmail.com>
2022-10-09 07:52:15 -0400

    Initial commit

---
+++ LinkedList.js
@@ -0,0 +1,27 @@
+import Node from './Node.js';
+
+export default LinkedList = (function () {
+  function append(value) {}
+
+  function prepend(value) {}
+
+  function size() {}
+
+  function head() {}
+
+  function tail() {}
+
+  function at(index) {}
+
+  function pop() {}
+
+  function contains(value) {}
+
+  function find(value) {}
+
+  function toString() {}
+
+  function insertAt(value, index) {}
+
+  function removeAt(index) {}
+})();
---
+++ Node.js
@@ -0,0 +1,6 @@
+export default Node = (function () {
+  return {
+    value: null,
+    nexNode: null,
+  };
+})();
