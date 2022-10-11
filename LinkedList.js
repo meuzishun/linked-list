e69e557f01f050129f8d12b3f553103a497793b3
Andrew Smith <meuzishun@gmail.com>
2022-10-11 07:05:39 -0400

    Complete linked list with factory functions

--- LinkedList.js
+++ LinkedList.js
@@ -1,14 +1,15 @@
 import { Node } from './Node.js';
 
 export function LinkedList() {
-  let head = null;
+  let listHead = null;
+
   function append(value) {
     const node = Node();
     node.value = value;
-    if (head === null) {
-      head = node;
+    if (listHead === null) {
+      listHead = node;
     } else {
-      let curr = head;
+      let curr = listHead;
       while (curr.nextNode !== null) {
         curr = curr.nextNode;
       }
@@ -19,18 +20,17 @@ export function LinkedList() {
   function prepend(value) {
     const node = Node();
     node.value = value;
-    if (size() === 0) {
-      head = node;
-    }
-    if (size() !== 0) {
-      node.nextNode = head;
-      head = node;
+    if (listHead === null) {
+      listHead = node;
+    } else {
+      node.nextNode = listHead;
+      listHead = node;
     }
   }
 
   function size() {
     let count = 0;
-    let curr = head;
+    let curr = listHead;
     while (curr !== null) {
       count++;
       curr = curr.nextNode;
@@ -38,21 +38,59 @@ export function LinkedList() {
     return count;
   }
 
-  // function head() {}
+  function head() {
+    return listHead;
+  }
 
-  function tail() {}
+  function tail() {
+    let curr = listHead;
+    while (curr.nextNode !== null) {
+      curr = curr.nextNode;
+    }
+    return curr;
+  }
 
-  function at(index) {}
+  function at(index) {
+    let curr = listHead;
+    let i = 0;
+    while (i < index) {
+      i++;
+      curr = curr.nextNode;
+    }
+    return curr;
+  }
 
-  function pop() {}
+  function pop() {
+    let curr = listHead;
+    while (curr.nextNode.nextNode !== null) {
+      curr = curr.nextNode;
+    }
+    curr.nextNode = null;
+  }
 
-  function contains(value) {}
+  function contains(value) {
+    let curr = listHead;
+    while (curr !== null) {
+      if (curr.value === value) return true;
+      curr = curr.nextNode;
+    }
+    return false;
+  }
 
-  function find(value) {}
+  function find(value) {
+    let i = 0;
+    let curr = listHead;
+    while (curr !== null) {
+      if (curr.value === value) return i;
+      i++;
+      curr = curr.nextNode;
+    }
+    return null;
+  }
 
   function toString() {
     let str = '';
-    let curr = head;
+    let curr = listHead;
     while (curr !== null) {
       str += `( ${curr.value} ) -> `;
       curr = curr.nextNode;
@@ -61,14 +99,41 @@ export function LinkedList() {
     return str;
   }
 
-  function insertAt(value, index) {}
+  function insertAt(value, index) {
+    const node = Node();
+    node.value = value;
+    let curr = listHead;
+    let i = 0;
+    while (i < index - 1) {
+      curr = curr.nextNode;
+      i++;
+    }
+    node.nextNode = curr.nextNode;
+    curr.nextNode = node;
+  }
 
-  function removeAt(index) {}
+  function removeAt(index) {
+    let curr = listHead;
+    let i = 0;
+    while (i < index - 1) {
+      curr = curr.nextNode;
+      i++;
+    }
+    curr.nextNode = curr.nextNode.nextNode;
+  }
 
   return {
     append,
     prepend,
     size,
+    head,
+    tail,
+    at,
+    pop,
+    contains,
+    find,
     toString,
+    insertAt,
+    removeAt,
   };
 }
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import { LinkedList } from './LinkedList.js';
 
 const myList = LinkedList();
+console.log(myList.toString());
 myList.append('Billy');
 myList.append(2);
 myList.append(3.14);
 myList.prepend('Andrew');
-console.log(myList.size());
+myList.append(123);
+// console.log(myList.size());
+// console.log(myList.tail());
+console.log(myList.head());
+// console.log(myList.at(2));
+// myList.pop();
+// console.log(myList.toString());
+// console.log(myList.contains('Andrew'));
+// console.log(myList.find(3.14));
+console.log(myList.toString());
+myList.insertAt('Car', 2);
+console.log(myList.toString());
+myList.removeAt(4);
 console.log(myList.toString());
